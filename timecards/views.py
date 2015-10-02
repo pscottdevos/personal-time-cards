85d64a261f709ffccfb81ea4c8a1f245ad8dad29
pscottdevos <scott.devos@sykes.com>
2015-10-02 10:14:48 -0500

    add pluggable writer for reports and add excel writer
    also make report view more modular

--- timecards/views.py
+++ timecards/views.py
@@ -2,15 +2,53 @@ import csv
 import logging
 import traceback
 from datetime import date, timedelta
+
+from django.conf import settings
 from django.db.models import Count
 from django.http import HttpResponse
 from django.shortcuts import render
+
 from timecards import models
+
+from openpyxl import Workbook
+from openpyxl.writer.excel import save_virtual_workbook
 
 
 logger = logging.getLogger(__name__)
 
-# Create your views here.
+writer_name = getattr(settings, 'WRITER', 'csv')
+
+if writer_name == 'csv':
+
+    class Writer(object):
+        content_type = 'text/csv'
+        extension='csv'
+        def __init__(self, buf):
+            self.writer = csv.writer(buf)
+
+        def writerow(self, row):
+            self.writer.writerow(row)
+
+        def close(self):
+            None
+
+if writer_name == 'excel':
+
+    class Writer(object):
+        content_type = \
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        extension = 'xlsx'
+        def __init__(self, buf):
+            self.buffer = buf
+            self.workbook = Workbook()
+            self.worksheet = self.workbook.active
+
+        def writerow(self, row):
+            self.worksheet.append(row)
+
+        def close(self):
+            self.buffer.write(save_virtual_workbook(self.workbook))
+
 
 months = [
     'None', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
@@ -21,10 +59,11 @@ def week_name(week):
 
 def last_month_report_view(request):
 
-    response = HttpResponse(mimetype='text/csv')
-    response['Content-Disposition'] = 'attachment; filename=hours.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = ('attachment; filename=hours.{0}'
+        .format(Writer.extension))
 
-    writer = csv.writer(response)
+    writer = Writer(response)
 
     today = date.today()
     one_month_ago = models.subtract_from_date(today, months=1)
@@ -41,49 +80,60 @@ def last_month_report_view(request):
     weeks[4][1] = last_month_end
     if weeks[4][0] > last_month_end:
         weeks.pop()
-    
+
+    write_week_headers(writer, weeks)
+
+    write_week_rows(writer, weeks)
+    return response
+
+
+def write_week_headers(writer, weeks):
     writer.writerow([
         'Product', 'Component', 'Bug', 'Summary'] + [ week_name(week)
         for week in weeks ])
+
+def write_week_rows(writer, weeks):
     rows = []
     for week in weeks:
-        bugs = (models.TimeCard.objects
-            .filter(date__gte=week[0], date__lte=week[1])
-            .exclude(bug=None)
-            .values('bug')
-            .annotate(Count('id')).order_by('bug'))
-        week_rows = []
-        for bug in ([
-                models.get_bug(item['bug'])['bugs'][0] for item in bugs ] +
-                [ {
-                    'id': None,
-                    'product': '',
-                    'component': '',
-                    'summary': 'Overhead'} ]):
-            cards = models.TimeCard.objects.filter(
-                date__gte=week[0], date__lte=week[1], bug=bug['id'])
-            hours = sum([ card.hours for card in cards ])
-            week_rows.append(
-                [
-                    bug['product'].encode('utf-8'),
-                    bug['component'].encode('utf-8'),
-                    bug['id'], bug['summary'].encode('utf-8') ] +
-                [
-                    (hours if week == weeks[i] else '')
-                    for i, w in enumerate(weeks) ])
-        week_rows.sort()
-        rows += week_rows
+        rows += get_rows_for_week(week, weeks)
+
     for i, row in enumerate(rows):
-        print row[3], type(row[3])
         writer.writerow(row)
 
     writer.writerow(['', '', '', '',]  + [
         '=sum({0}2:{0}{1})'.format('EFGHIJ'[n], i+2)
         for n, w in enumerate(weeks) ] + [
         '=sum(E{0}:{1}{0})'.format(i+3, 'EFGHIJ'[len(weeks)-1]) ])
-    #[ writer.writerow(row) for row in rows ]
-    return response
+    writer.close()
 
+def get_rows_for_week(week, weeks):
+    start, end = week
+    bugs = (models.TimeCard.objects
+        .filter(date__gte=start, date__lte=end)
+        .exclude(bug=None)
+        .values('bug')
+        .annotate(Count('id')).order_by('bug'))
+    week_rows = []
+    for bug in ([
+            models.get_bug(item['bug'])['bugs'][0] for item in bugs ] +
+            [ {
+                'id': None,
+                'product': '',
+                'component': '',
+                'summary': 'Overhead'} ]):
+        cards = models.TimeCard.objects.filter(
+            date__gte=start, date__lte=end, bug=bug['id'])
+        hours = sum([ card.hours for card in cards ])
+        week_rows.append(
+            [
+                bug['product'].encode('utf-8'),
+                bug['component'].encode('utf-8'),
+                bug['id'], bug['summary'].encode('utf-8') ] +
+            [
+                (hours if week == weeks[i] else '')
+                for i, w in enumerate(weeks) ])
+    week_rows.sort()
+    return week_rows
 
 def update_bug_info_view(request):
     """Update the bug info on every card"""
